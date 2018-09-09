cb718b1ccda0aec66ffd81a351438a55184dc8b7
Brett Beutell <brbeut@gmail.com>
2018-09-09 20:07:04 +0200

    Get packaging to work on mac

--- src/webpack.config.renderer.prod.ts
+++ src/webpack.config.renderer.prod.ts
@@ -50,7 +50,7 @@ export default merge.smart(baseConfig as any, {
           use: {
             loader: "css-loader",
             options: {
-              modules: true,
+              // modules: true,
               minimize: true,
               importLoaders: 1,
               localIdentName: "[name]__[local]__[hash:base64:5]"
